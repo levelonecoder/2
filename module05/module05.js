e67222a3bf789c93ae2dd2a2d111a276d8c461c7
levelonecoder <49767516+levelonecoder@users.noreply.github.com>
2019-05-11 23:48:04 -0700

    Add files via upload

---
+++ module05/module05.js
@@ -0,0 +1,15 @@
+
+
+n=prompt("enter a number=","");
+i=1
+document.write("<h2>Multiplication table of "+n+"</h2>");
+while(i<=10)
+
+{
+document.write(n+ "*" +i+ "=" +n*i+"<br>");
+i=i+1;
+
+}
+
+//-->
+,/script>
