e975c63944bf0ff8b15b99b3502a22799e8b9cef
levelonecoder <49767516+levelonecoder@users.noreply.github.com>
2019-05-11 23:55:59 -0700

    Update module05.js

--- module05/module05.js
+++ module05/module05.js
@@ -1,15 +1,23 @@
 
+var table = prompt("Enter a value to multiply", 1);             // Unit of table
+var operator = 'multiplication'; // Type of calculation
+var i = 1;                 // Set counter to 1
+var msg = '<h2>Multiplication Table</h2>';              // Message
 
-n=prompt("enter a number=","");
-i=1
-document.write("<h2>Multiplication table of "+n+"</h2>");
-while(i<=10)
+//if (operator === 'addition') {
+  // Do addition
+ // while (i < 11) {
+  //  msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+   // i++;
+ // }
+//} else {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+ }
+//}
 
-{
-document.write(n+ "*" +i+ "=" +n*i+"<br>");
-i=i+1;
-
-}
-
-//-->
-,/script>
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg;
